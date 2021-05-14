658bcdd5dd3cca285d5a483b61e0f8f3506fca23
Andrew Lorbieski <andrew.lorbieski@gmail.com>
2021-05-13 21:43:43 -0500

    Fix augs in GEO idle cape

--- Onamihime_Geo_Gear.lua
+++ Onamihime_Geo_Gear.lua
@@ -10,7 +10,7 @@ function user_job_setup()
 	state.Weapons:options('None','Nehushtan','DualWeapons')
 
 	gear.nuke_jse_back = {name="Nantosuelta's Cape",augments={'INT+20','Mag. Acc+20 /Mag. Dmg.+20','"Mag.Atk.Bns."+10'}}
-	gear.idle_jse_back = {name="Nantosuelta's Cape",augments={'HP+60','Eva.+20 /Mag. Eva.+20','Pet: "Regen"+10'}}
+	gear.idle_jse_back = {name="Nantosuelta's Cape",augments={'Pet: "Regen"+10','Pet: "Regen"+5',}}
 	gear.mndenfeeb_jse_back = {name="Nantosuelta's Cape",augments={'MND+20','Mag. Acc+20 /Mag. Dmg.+20','"Fast Cast"+10'}}
 	
 	gear.obi_cure_back = "Tempered Cape +1"
