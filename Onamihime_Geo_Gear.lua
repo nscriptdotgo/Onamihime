0343c1391788f6fd9223970f96f018a01eef8748
Noah Dlugoszewski <noahjdlugoszewski@gmail.com>
2021-07-02 08:38:09 -0500

    fixed auto GEO spells

--- Onamihime_Geo_Gear.lua
+++ Onamihime_Geo_Gear.lua
@@ -22,8 +22,10 @@ function user_job_setup()
 	gear.obi_high_nuke_back = gear.nuke_jse_back
 	gear.obi_high_nuke_waist = "Refoccilation Stone"
 	
-	autoindi = "Haste"
-	autogeo = "Frailty"
+	autoindi = 'Haste'
+	autogeo = 'Frailty'w
+	autoentrust = 'Fury'
+	autoentrustee = 'Katagiri'
 	
 	-- Additional local binds
 	send_command('bind ^` gs c cycle ElementalMode')
